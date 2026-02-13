1639b093be88e71b49922ca054d2aa7b9bc507ba
Aim asademy <245927605+swekshabeauty-debug@users.noreply.github.com>
2026-02-13 20:56:05 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -18,5 +18,7 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 const db = getFirestore(app);
 const storage = getStorage(app);
 const auth = getAuth(app);
+import { GoogleAuthProvider } from 'firebase/auth';
+const googleProvider = new GoogleAuthProvider();
 
-export { db, storage, auth };
+export { db, storage, auth, googleProvider };
